13b7bc53f6aadc1d8298bd6eb0bc8f33916faf42
Tariquzzaman-Faisal <tariquzzaman@iut-dhaka.edu>
2023-03-08 16:26:26 +0600

    scaper works for amazon uk, some comments added

--- scraper/main.py
+++ scraper/main.py
@@ -6,9 +6,8 @@ class Reviews:
         self.session = HTMLSession()
         self.headers = {'User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
         self.url = f'https://www.amazon.co.uk/product-reviews/{self.asin}/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&sortBy=recent&pageNumber='
-    def paganation(self, page):
+    def paganation(self, page): # iterates through the review pages
         r = self.session.get(self.url+str(page))
-        # r.html.render(sleep=1)
         if not r.html.find('div[data-hook=review]'):
             return False
         else:
@@ -16,9 +15,9 @@ class Reviews:
     def parse(self, reviews):
         total = []
         for review in reviews:
-            title = review.find('a[data-hook=review-title]', first = True).text
-            rating = review.find('i[data-hook=review-star-rating] span', first = True).text
-            body = review.find('span[data-hook=review-body] span', first = True).text.replace('\n', '').strip()
+            title = review.find('a[data-hook=review-title]', first = True).text # takes the first item from the returned list
+            rating = review.find('i[data-hook=review-star-rating] span', first = True).text # same
+            body = review.find('span[data-hook=review-body] span', first = True).text.replace('\n', '').strip() # same
 
             data = {
                 'title': title,
@@ -28,7 +27,7 @@ class Reviews:
             total.append(data)
         return total
 
-if __name__ == '__main__':
+if __name__ == '__main__': # check whether it is being called directly or called by importing
     amz = Reviews('B0797QCXS6')
     reviews = amz.paganation(1)
     print(amz.parse(reviews))
